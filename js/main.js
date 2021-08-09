0bee9fea284b593826ab613d3a8a11cfa6702744
Sam Perry <snperry89@gmail.com>
2021-08-09 18:07:13 -0400

    New Portfolio

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// // Get the modal
+// var modal = document.getElementsByClassName('contact-modal-lg');
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+// $('#button').submit(function(e) {
+//     e.preventDefault();
+//     // Coding
+//     $('#IDModal').modal('toggle'); //or  $('#IDModal').modal('hide');
+//     return false;
+// });
+
+
+$('#contact-modal-lg').modal('hide');
